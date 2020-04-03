e65cb1cb2de546596cd7b67c083b0553b28cf3f7
Jordan Gustafson <jordanrgustafson@gmail.com>
2020-04-02 20:05:33 -0500

    Add hide keyboard button

--- Sources/UIComponents/AdjustsForKeyboard.swift
+++ Sources/UIComponents/AdjustsForKeyboard.swift
@@ -9,38 +9,78 @@ import UIKit
 import SwiftUI
 import Combine
 
-
 //From https://stackoverflow.com/a/57147043
 public struct AdjustsForKeyboard: ViewModifier {
+    
+    private let showsHideKeyboardButton: Bool
+    @ObservedObject private var keyboardListener: KeyboardListener
+    
+    public init(showsHideKeyboardButton: Bool = true) {
+        self.showsHideKeyboardButton = showsHideKeyboardButton
+        self.keyboardListener = KeyboardListener()
+    }
+    
+    public func body(content: Content) -> some View {
+        GeometryReader { proxy in
+            VStack {
+                content
+                    .overlay(self.hideKeyboardButtonView(), alignment: .bottomTrailing)
+                    .frame(height: (proxy.size.height - self.keyboardListener.keyboardHeight), alignment: .top)
+                Spacer()
+            }
+        }
+        .animation(.easeInOut(duration: 0.3))
+    }
+    
+    private func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
 
-    @State var currentHeight: CGFloat = 0
+    private func hideKeyboardButtonView() -> some View {
+        if keyboardListener.keyboardHeight > 0.0 {
+            return AnyView(Button(action: { self.hideKeyboard() }) {
+                Image(systemName: "keyboard.chevron.compact.down")
+                    .padding()
+                    .background(Color(UIColor.secondarySystemBackground))
+                    .clipShape(Circle())
+                    .accentColor(.orange)
+            })
+        } else {
+            return AnyView(Spacer())
+        }
+    }
+
+}
+
+fileprivate class KeyboardListener: ObservableObject {
+ 
+    @Published var keyboardHeight: CGFloat = 0
+    
+    private var keyboardCancelables: Set<AnyCancellable> = Set()
     
     private let keyboardWillOpen = NotificationCenter.default
         .publisher(for: UIResponder.keyboardWillShowNotification)
         .map { $0.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! CGRect }
         .map { $0.height }
-
+    
     private let keyboardWillHide =  NotificationCenter.default
         .publisher(for: UIResponder.keyboardWillHideNotification)
         .map { _ in CGFloat.zero }
     
-    public init() {
-        
+    init() {
+        subscribeToKeyboardEvents()
     }
-
-    public func body(content: Content) -> some View {
-        content
-            .padding(.bottom, currentHeight)
-            .edgesIgnoringSafeArea(currentHeight == 0 ? Edge.Set() : .bottom)
-            .onAppear(perform: subscribeToKeyboardEvents)
-    }
-
+    
     private func subscribeToKeyboardEvents() {
-        _ = keyboardWillOpen
+        keyboardWillOpen
             .merge(with: keyboardWillHide)
             .subscribe(on: RunLoop.main)
-            .assign(to: \.currentHeight, on: self)
-        
+            .sink(receiveValue: { height in
+                withAnimation {
+                    self.keyboardHeight = height
+                }
+            })
+            .store(in: &keyboardCancelables)
     }
     
 }
